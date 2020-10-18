3b8689152d1907f2238d1917d2d4def5d5c3e8e4
Ajden Towfeek <ajden.towfeek@gmail.com>
2020-10-18 19:42:38 +0200

    After Customizing the Form

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewContactDialogComponent implements OnInit {
 
-  constructor() { }
+  avatars = [
+    'svg-1', 'svg-2', 'svg-3', 'svg-4'
+  ];
+
+  user: User;
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
 
   ngOnInit(): void {
+    this.user = new User();
+  }
+
+  save() {
+    this.dialogRef.close(this.user);
+  }
+
+  dismiss() {
+    this.dialogRef.close(null);
   }
 
 }
